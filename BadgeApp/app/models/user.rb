80e01d334e8e126282d5dfdc3d1060a659ca6520
skhakimov <samir.hakim@nyu.edu>
2015-09-12 09:49:00 -0400

    Password validation

--- BadgeApp/app/models/user.rb
+++ BadgeApp/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
                     uniqueness: {case_sensitive: false}
 
   has_secure_password
-  # validates :password, presence: true, length: {minimum: 7}
+  validates :password, presence: true, length: {minimum: 7}
 
   # Returns the hash digest of the given string.
   def User.digest(string)

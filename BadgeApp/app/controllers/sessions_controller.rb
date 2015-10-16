3e4a005303d3eadf04487b41d37991fb67b16b86
skhakimov <samir.hakim@nyu.edu>
2015-10-16 18:23:48 -0400

    Github Users are able to see ALL repos they can commit to, closes #48

--- BadgeApp/app/controllers/sessions_controller.rb
+++ BadgeApp/app/controllers/sessions_controller.rb
@@ -18,6 +18,7 @@ class SessionsController < ApplicationController
     else
       auth = request.env["omniauth.auth"]
       user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+      session[:user_token] = auth["credentials"]["token"]
       log_in user
       flash[:success] = "Signed in!"
       redirect_to root_url

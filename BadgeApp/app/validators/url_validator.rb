466e8b84195bffdbb6024b4dfa9b69be28368e8b
Dan Kohn <dan@dankohn.com>
2015-10-17 17:00:26 -0400

    Working validations

--- BadgeApp/app/validators/url_validator.rb
+++ BadgeApp/app/validators/url_validator.rb
@@ -2,14 +2,13 @@ class UrlValidator < ActiveModel::EachValidator
   # The URL validation rules are somewhat overly strict, but should serve;
   # the idea is to prevent attackers from inserting redirecting URLs
   # that can sometimes be used to attack (e.g., "?...", or ones with <).
-  URL_REGEX = %r{
-    \A(|https?:\/\/[A-Za-z0-9][-A-Za-z0-9_.\/]*(\/[-A-Za-z0-9_.\/\+,#]*)?)\z
-    }.freeze
-  URL_MESSAGE = 'URL must begin with http: or https: and use a limited' \
+  URL_REGEX =
+    %r{\A(|https?:\/\/[A-Za-z0-9][-A-Za-z0-9_.\/]*(\/[-A-Za-z0-9_.\/\+,#]*)?)\z}
+    .freeze
+  URL_MESSAGE = 'must begin with http: or https: and use a limited' \
                 ' charset'.freeze
 
   def validate_each(record, attribute, value)
-    return
     return if value =~ URL_REGEX
     record.errors.add attribute, (options[:message] || URL_MESSAGE)
   end
--- BadgeApp/test/test_helper.rb
+++ BadgeApp/test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport
+module ActiveSupport
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
     # order.

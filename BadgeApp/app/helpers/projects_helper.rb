4242f57294cdfe5773c3f532ae19cbe8fb3d9547
Dan Kohn <dan@dankohn.com>
2015-10-18 01:58:07 -0400

    Sort Github list by original an forked; fixes #94

---
+++ BadgeApp/app/helpers/projects_helper.rb
@@ -0,0 +1,24 @@
+module ProjectsHelper
+  def github_select
+    # List original then forked Github projects, with headers
+    fork_repos, original_repos = fork_and_original
+    original_header(original_repos) + original_repos +
+      fork_header(fork_repos) + fork_repos
+  end
+
+  def fork_and_original
+    github = Github.new oauth_token: session[:user_token]
+    repo_data = github.repos.list.map do |repo|
+      [repo.full_name, repo.fork, repo.html_url]
+    end
+    repo_data.partition { |repo| repo[1] } # partition on fork
+  end
+
+  def original_header(original_repos)
+    original_repos.blank? ? [] : [['=> Original Github Repos', '', '']]
+  end
+
+  def fork_header(fork_repos)
+    fork_repos.blank? ? [] : [['=> Forked Github Repos', '', '']]
+  end
+end

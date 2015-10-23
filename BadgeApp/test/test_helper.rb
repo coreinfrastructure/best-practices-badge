9035627bcab7e9bd64dd47c37b7fbfd67045a106
Dan Kohn <dan@dankohn.com>
2015-10-22 21:31:47 -0400

    Manually fix merge

--- BadgeApp/test/test_helper.rb
+++ BadgeApp/test/test_helper.rb
@@ -14,5 +14,24 @@ module ActiveSupport
     def logged_in?
       !session[:user_id].nil?
     end
+
+    # Logs in a test user.
+    def log_in_as(user, options = {})
+      password = options[:password] || 'password'
+      if integration_test?
+        post login_path, session: { email:       user.email,
+                                    password:    password,
+                                    remember_me: remember_me }
+      else
+        session[:user_id] = user.id
+      end
+    end
+
+    private
+
+    # Returns true inside an integration test.
+    def integration_test?
+      defined?(post_via_redirect)
+    end
   end
 end

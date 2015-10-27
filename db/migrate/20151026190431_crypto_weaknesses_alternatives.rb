72fa8b6c8240117d1b4e0db880bc13aaa8ccf7cf
David A. Wheeler <dwheeler@dwheeler.com>
2015-10-27 15:44:31 -0400

    Add migration for new fields: crypto weaknesses and crypto alternatives

---
+++ db/migrate/20151026190431_crypto_weaknesses_alternatives.rb
@@ -0,0 +1,9 @@
+class CryptoWeaknessesAlternatives < ActiveRecord::Migration
+  def change
+    add_column :projects, :crypto_weaknesses_status, :string, default: '?'
+    add_column :projects, :crypto_weaknesses_justification, :text
+
+    add_column :projects, :crypto_alternatives_status, :string, default: '?'
+    add_column :projects, :crypto_alternatives_justification, :text
+  end
+end

c55f6ec4ad0eb5a87c6d03a9a9d3ca5e065668fd
David A. Wheeler <dwheeler@dwheeler.com>
2015-11-06 17:23:00 -0500

    Add new integration test file for manipulating projects

---
+++ test/integration/users_manipulate_project_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersManipulateProjectTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:test_user)
+  end
+
+  test 'logged-in user adds a project' do
+    log_in_as @user
+
+    get '/projects/new'
+    assert_response :success
+    assert_template 'projects/new'
+
+    #  assert_select 'a[href=?]', login_path, count: 0
+    #  assert_select 'a[href=?]', logout_path
+    #  assert_select 'a[href=?]', user_path(@user)
+    #  delete logout_path
+    #  assert_not logged_in?
+    #  assert_redirected_to root_url
+    #  follow_redirect!
+    #  assert_select 'a[href=?]', login_path
+    #  assert_select 'a[href=?]', logout_path,      count: 0
+    #  assert_select 'a[href=?]', user_path(@user), count: 0
+  end
+end

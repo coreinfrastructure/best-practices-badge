8e70dfa3db7177640b6171d34b6c583311aed610
David A. Wheeler <dwheeler@dwheeler.com>
2015-11-28 09:31:15 -0500

    Re-enable autofill for testing

--- test/integration/users_manipulate_project_test.rb
+++ test/integration/users_manipulate_project_test.rb
@@ -14,15 +14,28 @@ class UsersManipulateProjectTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template 'projects/new'
 
-    #  assert_select 'a[href=?]', login_path, count: 0
-    #  assert_select 'a[href=?]', logout_path
-    #  assert_select 'a[href=?]', user_path(@user)
-    #  delete logout_path
-    #  assert_not logged_in?
-    #  assert_redirected_to root_url
-    #  follow_redirect!
-    #  assert_select 'a[href=?]', login_path
-    #  assert_select 'a[href=?]', logout_path,      count: 0
-    #  assert_select 'a[href=?]', user_path(@user), count: 0
+    repo_url = 'https://github.com/linuxfoundation/cii-best-practices-badge'
+
+    VCR.use_cassette('users_manipulate_test') do
+      post '/projects',
+           'project[project_homepage_url]' =>  repo_url,
+           'project[repo_url]' => repo_url
+      assert_response :redirect
+      follow_redirect!
+
+      assert_response :success
+      assert_template 'projects/edit'
+
+      #  assert_select 'a[href=?]', login_path, count: 0
+      #  assert_select 'a[href=?]', logout_path
+      #  assert_select 'a[href=?]', user_path(@user)
+      #  delete logout_path
+      #  assert_not logged_in?
+      #  assert_redirected_to root_url
+      #  follow_redirect!
+      #  assert_select 'a[href=?]', login_path
+      #  assert_select 'a[href=?]', logout_path,      count: 0
+      #  assert_select 'a[href=?]', user_path(@user), count: 0
+    end
   end
 end

77d35fac06f1d0effa7523cb21bc6d118a6ba01d
David A. Wheeler <dwheeler@dwheeler.com>
2015-12-22 17:35:08 -0500

    Modify production environment to support sending email using SendGrid
      - This modifies the production environment configuration so it
        can send email.  This this URL for the general approach:
        https://www.railstutorial.org/book/account_activation_password_reset
        We ran 'heroku addons:create sendgrid:starter' and then added
        these lines so that routines for sending email are available.
        This commit does *NOT* add any code to actually send email,
        this just puts some basic infrastructure in place so that we CAN.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,22 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to
-  # raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # Configure email server.
+  # For discussion about how to do this, see:
+  # https://www.railstutorial.org/book/account_activation_password_reset
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'secret-retreat-6638.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

ca47d1378cc57a236d1d3e54e00ef81717c83cc1
David A. Wheeler <dwheeler@dwheeler.com>
2016-02-19 13:10:25 -0500

    Include URL in emailed badge update reports.  Fixes #225
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 class ReportMailer < ApplicationMailer
   def project_status_change(project, old_badge_status, new_badge_status)
     @project = project
     @old_badge_status = old_badge_status
     @new_badge_status = new_badge_status
+    @project_info_url = ('https://' + (ENV['PUBLIC_HOSTNAME'] || 'localhost') +
+                        '/projects/' + @project.id.to_s).freeze
     @report_destination = 'cii-badge-log@lists.coreinfrastructure.org'
     mail(to: @report_destination,
          subject: "Project #{project.id} status change to " \

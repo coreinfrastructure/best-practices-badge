c39f53041d693906f2279cebf0a3a9fba466b6cb
David A. Wheeler <dwheeler@dwheeler.com>
2016-02-29 15:00:03 -0500

    Revert secure_headers.rb.
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,17 +1,16 @@
 SecureHeaders::Configuration.default do |config|
   normal_src = %w('self')
-  config.x_permitted_cross_domain_policies = 'none'
   if ENV['PUBLIC_HOSTNAME']
     fastly_alternate = 'https://' + ENV['PUBLIC_HOSTNAME'] +
                        '.global.ssl.fastly.net'
     normal_src += [fastly_alternate]
-    config.x_permitted_cross_domain_policies = fastly_alternate
   end
   config.hsts = "max-age=#{20.years.to_i}"
   config.x_frame_options = 'DENY'
   config.x_content_type_options = 'nosniff'
   config.x_xss_protection = '1; mode=block'
   config.x_download_options = 'noopen'
+  config.x_permitted_cross_domain_policies = 'none'
   # Configure CSP
   config.csp = {
     default_src: normal_src,

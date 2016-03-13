90e0f2669be22ce2b4e2fd0bd3d04c0d2afc8b30
David A. Wheeler <dwheeler@dwheeler.com>
2016-03-13 18:33:29 -0400

    Use 'bullet' during testing
      If an N+1 query is detected during testing, cause a crash.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,4 +42,30 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.after_initialize do
+    # The 'bullet' gem watches application queries and notifies
+    # when you should add eager loading (N+1 queries),
+    # when you're using eager loading that isn't necessary and
+    # when you should use counter cache.
+    Bullet.enable = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+    Bullet.raise = true # raise an error if n+1 query occurs
+    # Bullet.alert = true
+    # Bullet.bullet_logger = true
+    # Bullet.console = true
+    # Bullet.growl = true
+    # Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
+    #               :password => 'bullets_password_for_jabber',
+    #               :receiver => 'your_account@jabber.org',
+    #               :show_online_status => true }
+    # Bullet.honeybadger = true
+    # Bullet.bugsnag = true
+    # Bullet.airbrake = true
+    # Bullet.rollbar = true
+    # Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+    # Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware' ]
+    # Bullet.slack = { webhook_url: 'http://some.slack.url', foo: 'bar' }
+  end
 end

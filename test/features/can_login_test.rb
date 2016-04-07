9fe2372daa907077c02883c3ed40e267c02d1690
skhakimov <samir.hakim@nyu.edu>
2016-04-06 21:39:45 -0400

    Add more feature tests
    Signed-off-by: skhakimov <samir.hakim@nyu.edu>

--- test/features/can_login_test.rb
+++ test/features/can_login_test.rb
@@ -1,16 +1,27 @@
 require 'test_helper'
 
 class CanLoginTest < Capybara::Rails::TestCase
+  setup do
+    @user = users(:test_user)
+    @project = projects(:one)
+  end
+
   scenario 'Has link to GitHub Login', js: true do
     visit login_path
     page.must_have_content('Log in with GitHub')
   end
 
-  scenario 'Can Login using custom account', js: true do
+  scenario 'Can Login and edit using custom account', js: true do
     visit login_path
-    fill_in 'Email', with: 'test@example.org'
+    fill_in 'Email', with: @user.email
     fill_in 'Password', with: 'password'
     click_button 'Log in using custom account'
     assert page.has_content? 'Signed in!'
+    visit edit_project_path(@project)
+    choose 'project_english_status_met'
+    assert page.find('#english_enough')['src'].include? 'Thumbs_up'
+    click_on 'Reporting'
+    choose 'project_report_process_status_unmet'
+    assert page.find('#report_process_enough')['src'].include? 'Thumbs_down'
   end
 end

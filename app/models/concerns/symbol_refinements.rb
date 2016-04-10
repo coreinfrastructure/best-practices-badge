a32402b6269599ead0bf8027e068144a6f15152f
Dan Kohn <dan@dankohn.com>
2016-04-10 16:09:06 -0400

    Revert "Memoize symbol concern"
    This reverts commit 62b7aaf0b17e5772e9d65311d189fbc38efa8c6b.

--- app/models/concerns/symbol_refinements.rb
+++ app/models/concerns/symbol_refinements.rb
@@ -2,11 +2,11 @@
 module SymbolRefinements
   refine Symbol do
     def status
-      @status ||= "#{self}_status".to_sym
+      "#{self}_status".to_sym
     end
 
     def justification
-      @justification ||= "#{self}_justification".to_sym
+      "#{self}_justification".to_sym
     end
   end
 end

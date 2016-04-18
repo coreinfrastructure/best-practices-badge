e381b5c086b430a454795e3cea1248569c8bdf8d
David A. Wheeler <dwheeler@dwheeler.com>
2016-04-18 14:46:25 -0400

    Remove unnecessary scroll_to_see calls in Capybara tests
      Now that we're using a different driver we no longer need to
      force scrolling to the right place - the driver does that for us.
      So, remove the calls for it.  We'll leave the scrolling method
      in place for now, in case its need returns.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- test/features/can_login_test.rb
+++ test/features/can_login_test.rb
@@ -28,19 +28,14 @@ class CanLoginTest < Capybara::Rails::TestCase
     assert page.has_content? 'Signed in!'
 
     visit edit_project_path(@project)
-    scroll_to_see('project_discussion_status_unmet')
     choose 'project_discussion_status_unmet'
-    scroll_to_see('discussion_enough')
     assert page.find('#discussion_enough[src*="result_symbol_x"]')
 
-    scroll_to_see('project_english_status_met')
     choose 'project_english_status_met'
     assert page.find('#english_enough[src*="result_symbol_check"]')
 
-    scroll_to_see('logo')
     click_on 'Change Control'
     assert page.has_content? 'repo_public'
-    scroll_to_see('project_repo_public_status_unmet')
     choose 'project_repo_public_status_unmet'
     assert page.find('#repo_public_enough[src*="result_symbol_x"]')
 

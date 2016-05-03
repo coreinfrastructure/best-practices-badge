a656db6aea256f6227b1e342260ea4a608b74071
David A. Wheeler <dwheeler@dwheeler.com>
2016-05-03 08:16:42 -0400

    Modify scheme.rb to enable_extension "pg_stat_statements"
      Added as part of database migration. See:
      https://devcenter.heroku.com/articles/
      heroku-postgres-extensions-postgis-full-text-search#pg_stat_statements
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20160501224428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "projects", force: :cascade do |t|
     t.integer  "user_id"

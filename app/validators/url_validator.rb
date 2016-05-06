2392ea2f021b02240de48447853a298ee3dce8b1
Dan Kohn <dan@dankohn.com>
2016-05-06 12:03:07 -0400

    Minor refactor (#329)
    * Minor refactor
    
    * Add tests
    
    Signed-off-by: Dan Kohn <dan@dankohn.com>
    
    * Fix capitalization

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -2,8 +2,7 @@ class UrlValidator < ActiveModel::EachValidator
   # The URL validation rules are somewhat overly strict, but should serve;
   # the idea is to prevent attackers from inserting redirecting URLs
   # that can sometimes be used to attack (e.g., "?...", or ones with <).
-  URL_REGEX =
-    %r{\A(|https?:\/\/[A-Za-z0-9][-A-Za-z0-9_.\/]*(\/[-A-Za-z0-9_.\/\+,#]*)?)\z}
+  URL_REGEX = /#{URI.regexp(%w(http https))}/
   URL_MESSAGE = 'must begin with http: or https: and use a limited' \
                 ' charset'.freeze
 

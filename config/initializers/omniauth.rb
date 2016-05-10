8eb753980cfb3a03f93056005e91fc4e0b080d7b
Dan Kohn <dan@dankohn.com>
2016-05-10 16:56:06 -0400

    Revert "WIP"
    This reverts commit b4a30ca7f9bd04ec12a33330668a836e2be80e71.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.test?
-    # Test app OAuth returns to a different port
-    ENV['GITHUB_KEY'] = ENV['TEST_GITHUB_KEY']
-    ENV['GITHUB_SECRET'] = ENV['TEST_GITHUB_SECRET']
-  end
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],
            scope: 'user:email, read:org'
 end

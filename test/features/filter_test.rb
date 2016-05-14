128a85b0cee53d5150d3546a7f845390fcdd6a15
Dan Kohn <dan@dankohn.com>
2016-05-14 08:50:18 -0700

    Passing tests

--- test/features/filter_test.rb
+++ test/features/filter_test.rb
@@ -9,46 +9,56 @@ class FilterTest < Capybara::Rails::TestCase
     assert has_content? 'Unjustified perfect project'
     assert has_content? 'Justified perfect project'
 
-    select 'Passing', from: 'status'
-    wait_for_url '/projects?status=passing'
+    select 'Passing', from: 'gteq'
+    wait_for_url '/projects?gteq=100'
     assert_equal 1, all('tbody tr').count
     assert has_no_content? 'Pathfinder OS'
     assert has_no_content? 'Mars Ascent Vehicle (MAV)'
     assert has_no_content? 'Unjustified perfect project'
     assert has_content? 'Justified perfect project'
 
-    select 'Failing', from: 'status'
-    wait_for_url '/projects?status=failing'
+    select 'In Progress (75% or more)', from: 'gteq'
+    wait_for_url '/projects?gteq=75'
+    assert_equal 2, all('tbody tr').count
+    assert has_no_content? 'Pathfinder OS'
+    assert has_no_content? 'Mars Ascent Vehicle (MAV)'
+    assert has_content? 'Unjustified perfect project'
+    assert has_content? 'Justified perfect project'
+
+    fill_in 'q', with: 'unjustified'
+    click_on 'Search'
+    wait_for_url '/projects?gteq=75&q=unjustified'
     assert_equal 1, all('tbody tr').count
     assert has_no_content? 'Pathfinder OS'
     assert has_no_content? 'Mars Ascent Vehicle (MAV)'
     assert has_content? 'Unjustified perfect project'
     assert has_no_content? 'Justified perfect project'
 
-    select 'In Progress', from: 'status'
-    wait_for_url '/projects?status=in_progress'
-    assert_equal 2, all('tbody tr').count
-    assert has_content? 'Pathfinder OS'
-    assert has_content? 'Mars Ascent Vehicle (MAV)'
-    assert has_no_content? 'Unjustified perfect project'
-    assert has_no_content? 'Justified perfect project'
-
-    fill_in 'q', with: 'mars'
-    click_on 'Search'
-    wait_for_url '/projects?q=mars&status=in_progress'
-    assert_equal 1, all('tbody tr').count
-    assert has_no_content? 'Pathfinder OS'
-    assert has_content? 'Mars Ascent Vehicle (MAV)'
-    assert has_no_content? 'Unjustified perfect project'
-    assert has_no_content? 'Justified perfect project'
-
     fill_in 'q', with: ''
     click_on 'Search'
-    wait_for_url '/projects?status=in_progress'
+    wait_for_url '/projects?gteq=75'
     assert_equal 2, all('tbody tr').count
+    assert has_no_content? 'Pathfinder OS'
+    assert has_no_content? 'Mars Ascent Vehicle (MAV)'
+    assert has_content? 'Unjustified perfect project'
+    assert has_content? 'Justified perfect project'
+
+    # No UI to see only In Progress projects
+    visit '/projects?status=in_progress'
+    wait_for_url '/projects?status=in_progress'
+    assert_equal 3, all('tbody tr').count
     assert has_content? 'Pathfinder OS'
     assert has_content? 'Mars Ascent Vehicle (MAV)'
-    assert has_no_content? 'Unjustified perfect project'
+    assert has_content? 'Unjustified perfect project'
+    assert has_no_content? 'Justified perfect project'
+
+    # Alternative URL see only In Progress projects
+    visit '/projects?lteq=99'
+    wait_for_url '/projects?lteq=99'
+    assert_equal 3, all('tbody tr').count
+    assert has_content? 'Pathfinder OS'
+    assert has_content? 'Mars Ascent Vehicle (MAV)'
+    assert has_content? 'Unjustified perfect project'
     assert has_no_content? 'Justified perfect project'
   end
 end

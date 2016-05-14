948dee866808dc440d71fb2662bf2cb4fbfa7ade
Dan Kohn <dan@dankohn.com>
2016-05-14 15:19:29 -0700

    Separately define passing and in progress badges

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -46,38 +46,54 @@ class Badge
 
   private
 
-  # rubocop:disable Metrics/CyclomaticComplexity
   def create_svg(percentage)
-    passing = percentage == 100
-    width = passing ? '192' : '204'
-    fill = passing ? '#97CA00' : '#dfb317'
-    d1 = passing ? 'M103 0h89v20h-89z' : 'M103 0h101v20H103z'
-    d2 = passing ? 'M0 0h192v20H0z' : 'M0 0h204v20H0z'
-    x = passing ? '145.5' : '152.5'
-    text = passing ? 'passing 100%' : "in progress #{percentage}%"
-    svg_template(width, fill, d1, d2, x, text)
+    return passing_svg if percentage == 100
+    in_progress_svg(percentage)
   end
-  # rubocop:enable Metrics/CyclomaticComplexity
 
-  # rubocop:disable Metrics/MethodLength, Metrics/ParameterLists
-  def svg_template(width, fill, d1, d2, x, text)
+  # rubocop:disable Metrics/MethodLength
+  def in_progress_svg(percentage)
     <<-ENDOFSTRING.squish
-    <svg xmlns="http://www.w3.org/2000/svg" width="#{width}"
+    <svg xmlns="http://www.w3.org/2000/svg" width="204"
     height="20"><linearGradient id="b" x2="0" y2="100%"><stop
     offset="0" stop-color="#bbb" stop-opacity=".1"/><stop
     offset="1" stop-opacity=".1"/></linearGradient><mask
-    id="a"><rect width="#{width}" height="20" rx="3"
+    id="a"><rect width="204" height="20" rx="3"
     fill="#fff"/></mask><g mask="url(#a)"><path fill="#555"
-    d="M0 0h103v20H0z"/><path fill="#{fill}"
-    d="#{d1}"/><path fill="url(#b)"
-    d="#{d2}"/></g><g fill="#fff" text-anchor="middle"
+    d="M0 0h103v20H0z"/><path fill="#dfb317" d="M103
+    0h101v20H103z"/><path fill="url(#b)" d="M0
+    0h204v20H0z"/></g><g fill="#fff" text-anchor="middle"
     font-family="DejaVu Sans,Verdana,Geneva,sans-serif"
     font-size="11"><text x="51.5" y="15" fill="#010101"
     fill-opacity=".3">cii best practices</text><text x="51.5"
-    y="14">cii best practices</text><text x="#{x}" y="15"
-    fill="#010101" fill-opacity=".3">#{text}</text><text
-    x="#{x}" y="14">#{text}</text></g></svg>
+    y="14">cii best practices</text><text x="152.5" y="15"
+    fill="#010101" fill-opacity=".3">in progress
+    #{percentage}%</text><text x="152.5" y="14">in progress
+    #{percentage}%</text></g></svg>
     ENDOFSTRING
   end
-  # rubocop:enable Metrics/MethodLength, Metrics/ParameterLists
+  # rubocop:enable Metrics/MethodLength
+
+  # rubocop:disable Metrics/MethodLength
+  def passing_svg
+    <<-ENDOFSTRING.squish
+    <svg xmlns="http://www.w3.org/2000/svg" width="192"
+    height="20"><linearGradient id="b" x2="0" y2="100%"><stop
+    offset="0" stop-color="#bbb" stop-opacity=".1"/><stop
+    offset="1" stop-opacity=".1"/></linearGradient><mask
+    id="a"><rect width="192" height="20" rx="3"
+    fill="#fff"/></mask><g mask="url(#a)"><path fill="#555"
+    d="M0 0h103v20H0z"/><path fill="#97CA00" d="M103
+    0h89v20h-89z"/><path fill="url(#b)" d="M0
+    0h192v20H0z"/></g><g fill="#fff" text-anchor="middle"
+    font-family="DejaVu Sans,Verdana,Geneva,sans-serif"
+    font-size="11"><text x="51.5" y="15" fill="#010101"
+    fill-opacity=".3">cii best practices</text><text x="51.5"
+    y="14">cii best practices</text><text x="145.5" y="15"
+    fill="#010101" fill-opacity=".3">passing
+    100%</text><text x="145.5" y="14">passing
+    100%</text></g></svg>
+    ENDOFSTRING
+  end
+  # rubocop:enable Metrics/MethodLength
 end

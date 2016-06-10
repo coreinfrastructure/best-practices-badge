9d3b547853420eed4b143961a3fd1c4229b68d98
David A. Wheeler <dwheeler@dwheeler.com>
2016-06-10 14:54:36 -0400

    Modify criteria.yml to include major/minor groups (#431)
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- config/initializers/criteria_hash.rb
+++ config/initializers/criteria_hash.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 require 'yaml'
-CriteriaHash = YAML.load_file('criteria.yml').with_indifferent_access.freeze
+# Load in entire criteria.yml, which keys off the major/minor groups
+FullCriteriaHash = YAML.load_file('criteria.yml').with_indifferent_access.freeze
+criteria_hash = {}.with_indifferent_access
+FullCriteriaHash.each do |major, major_value|
+  major_value.each do |minor, criteria|
+    criteria.each do |criterion_key, criterion_data|
+      criteria_hash[criterion_key] = criterion_data
+      criteria_hash[criterion_key][:major] = major
+      criteria_hash[criterion_key][:minor] = minor
+    end
+  end
+end
+CriteriaHash = criteria_hash.freeze

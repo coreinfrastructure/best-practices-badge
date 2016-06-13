f85a463105f0bfbc5f40d735206b12c69bace239
Dale Visser <dvisser@ida.org>
2016-06-13 15:40:46 -0400

    Possibly working SubdirFileContentsDetective, but have VCR-related failing test cases.

--- app/lib/subdir_file_contents_detective.rb
+++ app/lib/subdir_file_contents_detective.rb
@@ -11,57 +11,67 @@ class SubdirFileContentsDetective < Detective
     contribution_status license_location_status release_notes_status
     build_status build_common_tools_status
   ).freeze
-  FOLDER_PATTERN = /\Adoc(s|umentation)\Z/i
-  FILE_PATTERN = /(\.md|\.markdown|\.txt|\.html)?\Z/i
+  DOCS_BASICS = {
+    folder: /\Adoc(s|umentation)\Z/i,
+    file: /(\.md|\.markdown|\.txt|\.html)?\Z/i,
+    contents: [/install(ation)?/i, /us(e|ing)/i, /secur(e|ity)/i].freeze
+  }.freeze
 
-  def files_in_folder_named(_folder_files, _name_pattern)
-    false # TODO: add working code here
+  def unmet_result(result_description)
+    {
+      value: 'Unmet', confidence: 1,
+      explanation: "No #{result_description} file(s) found."
+    }
+  end
+
+  def met_result(result_description)
+    {
+      value: 'Met', confidence: 3,
+      explanation:
+        "Some #{result_description} file contents found."
+    }
+  end
+
+  def match_fso?(fso, type, name_pattern)
+    fso['type'] == type && fso['name'].match(name_pattern)
   end
 
   def folder_named(name_pattern)
     @top_level.select do |fso|
-      if fso['type'] == 'dir' && fso['name'].match(name_pattern)
-        return fso['name']
-      end
+      return fso['name'] if match_fso?(fso, 'dir', name_pattern)
     end
     nil
   end
 
-  def determine_content_results(
-          repo_files, status, required_contents, result_description
-  )
-    docs = folder_named(FOLDER_PATTERN)
-    if docs.nil?
-      @results[status] = unmet_result result_description
-    else
-      find_matching_content_in_files(
-        repo_files, required_contents, result_description
-      )
+  def match_file_content(files, patterns, description)
+    files = files.select { |f| match_fso?(f, 'file', patterns[:file]) }
+    files.select do |fso|
+      patterns[:content].each do |pattern|
+        return met_result description if fso['content'].match(pattern)
+      end
     end
+    unmet_result description
   end
 
-  def find_matching_content_in_files(
-          repo_files, required_contents, _result_description
-  )
-    found_files = files_in_folder_named(repo_files.get(docs), FILE_PATTERN)
-    required_contents.each do |pattern|
-      found_files # TODO: see below
-      pattern # TODO: code to look for pattern in found_files
-    end
+  def determine_content_results(repo_files, status, patterns, description)
+    folder = folder_named(patterns[:folder])
+    @results[status] =
+      if folder.nil?
+        unmet_result result_description
+      else
+        match_file_content(repo_files.get(folder), patterns, description)
+      end
   end
 
   def analyze(_evidence, current)
     repo_files = current[:repo_files]
     return {} if repo_files.blank?
-
     @results = {}
 
     # Top_level is iterable, contains a hash with name, size, type (file|dir).
     @top_level = repo_files.get_info('/')
-
     determine_content_results(
-      repo_files, :documentation_basics_status,
-      [/install(ation)?/i, /us(e|ing)/i, /secur(e|ity)/i],
+      repo_files, :documentation_basics_status, DOCS_BASICS,
       'documentation basics'
     )
   end

31659a657b5b14bf93e6989f2181cd3eb5bcbb4d
Mark Rader <msrader@gmail.com>
2016-10-02 13:48:18 -0500

    adding test

---
+++ test/unit/lib/build_detective_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class BuildDetectiveTest < ActiveSupport::TestCase
+  def setup
+    @full_name = 'linuxfoundation/cii-best-practices-badge'
+    @human_name = 'Core Infrastructure Initiative Best Practices Badge'
+    @evidence = Evidence.new({})
+    @repo_url = "https://github.com/#{@full_name}"
+  end
+
+  test 'Build' do
+    results = BuildDetective.new.analyze(
+      @evidence, repo_url: @repo_url
+    )
+    assert results == {}
+  end
+end

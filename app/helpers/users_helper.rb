45958dc46eff60cb115005617fd2f0acaff884e4
David A. Wheeler <dwheeler@dwheeler.com>
2016-10-27 14:23:42 -0400

    Use "mystery man" if there's no Gravatar image
    Use a simple (not distracting) "mystery man" image if there's
    no gravatar image for a given user.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   # Returns the Gravatar for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=mm"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
 end

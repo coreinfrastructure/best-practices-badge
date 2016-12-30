9a6dc3d206fbafdd76ce51be7ec2b9d4ec633e17
Dan Kohn <dan@dankohn.com>
2016-12-30 08:12:41 -0700

    Fix Rubocop errors
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require_relative 'boot'
 
 require 'rails/all'
@@ -8,7 +9,8 @@ Bundler.require(*Rails.groups)
 
 module BadgeApp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over those specified
+    # here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end

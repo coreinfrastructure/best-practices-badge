b7d1a50558e486931e0facf9915bc27715baae36
David A. Wheeler <dwheeler@dwheeler.com>
2017-01-21 09:48:18 -0500

    Reduce log noise in specific common cases
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- test/integration/no_page_test.rb
+++ test/integration/no_page_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 
 class NoPageTest < ActionDispatch::IntegrationTest
   test 'No such page returns 404' do
-    get '/there-is-no-such-page'
+    get '/wp-login.php'
     assert_response :missing
     assert_template 'static_pages/error_404'
   end

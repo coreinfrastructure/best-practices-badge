7ed2f6611e06a0f21b75fe73e2c427a1c68b7148
Jason Dossett <jdossett@utdallas.edu>
2017-01-23 15:39:06 -0500

    SubdirFileContentsDetective Fixes
      * Merges dwvisser-433-doc-basics-pr with current master
      * Resolves issues with github_login.yml
      * Adds tests to ensure coverage.
    
    Signed-off-by: Jason Dossett <jdossett@utdallas.edu>

--- test/unit/lib/subdir_file_contents_detective_test.rb
+++ test/unit/lib/subdir_file_contents_detective_test.rb
@@ -7,6 +7,10 @@ class SubdirFileContentsDetectiveTest < ActiveSupport::TestCase
     @human_name = 'Core Infrastructure Initiative Best Practices Badge'
     @evidence = Evidence.new({})
     @repo_url = "https://github.com/#{@full_name}"
+    @full_name2 = 'david-a-wheeler/test-badge-project'
+    @human_name2 = 'A test for transferring projects'
+    @evidence2 = Evidence.new({})
+    @repo_url = "https://github.com/#{@full_name2}"
   end
 
   test 'Subdir File Contents Detective Test' do
@@ -27,6 +31,27 @@ class SubdirFileContentsDetectiveTest < ActiveSupport::TestCase
       )
       assert dbs.key?(:value)
       assert_equal 'Met', dbs[:value]
+    end
+  end
+
+  test 'Subdir File Contents Detective Test for no subdir' do
+    VCR.use_cassette('unit_test_subdir_file_contents_detective_unmet') do
+      results = SubdirFileContentsDetective.new.analyze(
+        @evidence,
+        repo_files: GithubContentAccess.new(
+          'david-a-wheeler/test-badge-project',
+          proc { Octokit::Client.new }
+        )
+      )
+      assert results.key?(:documentation_basics_status)
+      dbs = results[:documentation_basics_status]
+      assert dbs.key?(:explanation)
+      assert_equal(
+        'No documentation basics file(s) found.',
+        dbs[:explanation]
+      )
+      assert dbs.key?(:value)
+      assert_equal 'Unmet', dbs[:value]
     end
   end
 end

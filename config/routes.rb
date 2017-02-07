24789572194fdff233b00f7724de0b0c6fc4f48d
Jason Dossett <jdossett@utdallas.edu>
2017-02-07 16:56:45 -0500

    Preserve edit in url on failed edit
    Signed-off-by: Jason Dossett <jdossett@utdallas.edu>

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: %i(new create edit update)
+  resources :projects
+  match 'projects/:id/edit' => 'projects#update', :via => [:put, :patch], :as => :put_project
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'

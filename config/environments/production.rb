95efdb813e25733fbed538658fd55109d0d4a452
Mark Rader <msrader@gmail.com>
2017-02-23 19:47:03 -0600

    caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.cache_store = :memory_store, { size: 64.megabytes }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.

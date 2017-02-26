56fdff98eb1990f40a42afc30d14712d4149410a
Mark Rader <msrader@gmail.com>
2017-02-26 08:36:20 -0600

    caching

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
   config.cache_store = :memory_store, { size: 64.megabytes }
 
   # Raise exceptions instead of rendering exception templates.

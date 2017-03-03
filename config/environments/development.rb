1dce2c7d6e02a4e16e3abf36c5b511c4ac025228
Mark Rader <msrader@gmail.com>
2017-03-02 19:10:37 -0600

    low level cacheing added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
   config.cache_store = :memory_store, { size: 64.megabytes }
 
   # Don't care if the mailer can't send.

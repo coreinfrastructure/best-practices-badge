8031e7172c0b20abf762a51eb6eb66360a6d9edf
David A. Wheeler <dwheeler@dwheeler.com>
2017-03-07 13:18:43 -0500

    Implement status_chooser cache to improve performance (#678)
    * Implement cache of status_chooser to improve performance.
      Required various changes for it to work correctly.
      Original developed by Mark Rader, then collaboratively developed.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,8 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.cache_store = :memory_store, { size: 64.megabytes }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true

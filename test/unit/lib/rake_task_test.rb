6bf341e9b348ee0de6133f220056a49bd6a4a180
Jason Dossett <jdossett@utdallas.edu>
2017-03-14 18:01:30 -0400

    Fix seed issue with rakeTaskTest regression test
    Signed-off-by: Jason Dossett <jdossett@utdallas.edu>

--- test/unit/lib/rake_task_test.rb
+++ test/unit/lib/rake_task_test.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 require 'test_helper'
+require 'database_cleaner'
+DatabaseCleaner.strategy = :transaction
 
 class RakeTaskTest < ActiveSupport::TestCase
+  # When using DatabaseCleaner, transactional fixtures must be off.
+  self.use_transactional_tests = false
+
+  setup do
+    # Start DatabaseCleaner before each test.
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    # Clean up the database with DatabaseCleaner after each test.
+    DatabaseCleaner.clean
+  end
+
   test 'regression test for rake reminders' do
     assert_equal 1, Project.projects_to_remind.size
     result = system 'rake reminders >/dev/null'

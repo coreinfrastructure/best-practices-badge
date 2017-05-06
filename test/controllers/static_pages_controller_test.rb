febd199b2e0693d7633af2c9112353a918c5fc10
David A. Wheeler <dwheeler@dwheeler.com>
2017-05-06 09:52:48 -0400

    Add tests to ensure locale is selecting correct static file

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,15 @@ class StaticPagesControllerTest < ActionController::TestCase
   test 'should get home' do
     get :home
     assert_response :success
+    assert_template 'home.en'
+
+    get :home, params: { locale: 'fr' }
+    assert_response :success
+    assert_template 'home.fr'
+
+    get :home, params: { locale: 'zh-CN' }
+    assert_response :success
+    assert_template 'home.zh-CN'
   end
 
   test 'should get background' do
@@ -16,5 +25,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test 'should get criteria' do
     get :criteria
     assert_response :success
+
+    get :criteria, params: { locale: 'fr' }
+    assert_response :success
+    assert_template 'criteria.fr'
+
+    get :criteria, params: { locale: 'zh-CN' }
+    assert_response :success
+    assert_template 'criteria.zh-CN'
   end
 end

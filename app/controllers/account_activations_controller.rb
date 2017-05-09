d55b3931b3b861550013aec45f05367abc79609c
David A. Wheeler <dwheeler@dwheeler.com>
2017-05-09 19:49:26 -0400

    Internationalize controllers account_activations and password_resets
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 class AccountActivationsController < ApplicationController
+  # rubocop:disable Metrics/AbcSize
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = 'Account activated!'
+      flash[:success] = t('account_activations.activated')
       redirect_to user
     else
-      flash[:danger] = 'Invalid activation link'
+      flash[:danger] = t('account_activations.failed_activation')
       redirect_to root_url
     end
   end
+  # rubocop:enable Metrics/AbcSize
 end

5b454164f61807f4ce5e8b24a57138002acaa2ca
David A. Wheeler <dwheeler@dwheeler.com>
2017-05-16 13:38:45 -0400

    Internationalize subject lines for user_mailer emails
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,47 +1,35 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.account_activation.subject
-  #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: 'Account activation'
+    I18n.with_locale(user.preferred_locale) do
+      mail to: user.email, subject: t('user_mailer.account_activation.subject')
+    end
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: 'Password reset'
+    I18n.with_locale(user.preferred_locale) do
+      mail to: user.email, subject: t('user_mailer.password_reset.subject')
+    end
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
   def user_update(user, changes)
     @user = user
     @changes = changes
     # If email changed, send to *both* email addresses (that way, if user
     # didn't approve this, the user will at least *see* the email change).
     destination = changes['email'] ? changes['email'] : user.email
-    mail to: destination, subject: 'User data edited'
+    I18n.with_locale(user.preferred_locale) do
+      mail to: destination, subject: t('user_mailer.user_update.subject')
+    end
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.github_welcome.subject
-  #
   def github_welcome(user)
     @user = user
-    mail to: user.email, subject: 'Welcome to the Badging Program'
+    I18n.with_locale(user.preferred_locale) do
+      mail to: user.email, subject: t('user_mailer.github_welcome.subject')
+    end
   end
 end

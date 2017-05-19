dcab0a627060dd7d9111122642fa24b62cb938ae
David A. Wheeler <dwheeler@dwheeler.com>
2017-05-18 22:15:47 -0400

    Pass a symbol (not string) to I18n.with_locale
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@
 class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
-    I18n.with_locale(user.preferred_locale) do
+    I18n.with_locale(user.preferred_locale.to_sym) do
       mail to: user.email, subject: t('user_mailer.account_activation.subject')
     end
   end
 
   def password_reset(user)
     @user = user
-    I18n.with_locale(user.preferred_locale) do
+    I18n.with_locale(user.preferred_locale.to_sym) do
       mail to: user.email, subject: t('user_mailer.password_reset.subject')
     end
   end
@@ -21,14 +21,14 @@ class UserMailer < ApplicationMailer
     # If email changed, send to *both* email addresses (that way, if user
     # didn't approve this, the user will at least *see* the email change).
     destination = changes['email'] ? changes['email'] : user.email
-    I18n.with_locale(user.preferred_locale) do
+    I18n.with_locale(user.preferred_locale.to_sym) do
       mail to: destination, subject: t('user_mailer.user_update.subject')
     end
   end
 
   def github_welcome(user)
     @user = user
-    I18n.with_locale(user.preferred_locale) do
+    I18n.with_locale(user.preferred_locale.to_sym) do
       mail to: user.email, subject: t('user_mailer.github_welcome.subject')
     end
   end

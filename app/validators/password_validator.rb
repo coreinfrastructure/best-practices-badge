0e01642189bd1d08439c4412b86b6b838871b3ae
Dan Kohn <dan@dankohn.com>
2017-06-03 17:56:01 -0400

    I18n validations (#769)
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- app/validators/password_validator.rb
+++ app/validators/password_validator.rb
@@ -8,6 +8,6 @@ class PasswordValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return unless BadPasswordSet.include?(value.downcase)
     record.errors.add attribute,
-                      options[:message] || 'is a well-known (bad) password'
+                      options[:message] || I18n.t('.known_bad_password')
   end
 end

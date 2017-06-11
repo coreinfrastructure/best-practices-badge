012f7a0e321e1e8de4eebb3cd3ca24651d8c1482
Dan Kohn <dan@dankohn.com>
2017-06-10 22:25:28 -0400

    Don't run bootsnap in production
    Signed-off-by: Dan Kohn <dan@dankohn.com>

--- config/boot.rb
+++ config/boot.rb
@@ -8,22 +8,24 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
-require 'bootsnap'
-Bootsnap.setup(
-  # Path to your cache
-  cache_dir:            'tmp/cache',
-  # This should be set to whatever evaluates your current working environment,
-  # e.g. RACK_ENV, RAILS_ENV, etc
-  development_mode:     ENV['RAILS_ENV'] == 'development',
-  # Should we optimize the LOAD_PATH with a cache?
-  load_path_cache:      true,
-  # Should we optimize ActiveSupport autoloads with cache?
-  autoload_paths_cache: true,
-  # Sets `RubyVM::InstructionSequence.compile_option =
-  #   { trace_instruction: false }`
-  disable_trace:        false,
-  # Should compile Ruby code into ISeq cache?
-  compile_cache_iseq:   true,
-  # Should compile YAML into a cache?
-  compile_cache_yaml:   true
-)
+unless Rails.env.production?
+  require 'bootsnap'
+  Bootsnap.setup(
+    # Path to your cache
+    cache_dir:            'tmp/cache',
+    # This should be set to whatever evaluates your current working environment,
+    # e.g. RACK_ENV, RAILS_ENV, etc
+    development_mode:     ENV['RAILS_ENV'] == 'development',
+    # Should we optimize the LOAD_PATH with a cache?
+    load_path_cache:      true,
+    # Should we optimize ActiveSupport autoloads with cache?
+    autoload_paths_cache: true,
+    # Sets `RubyVM::InstructionSequence.compile_option =
+    #   { trace_instruction: false }`
+    disable_trace:        false,
+    # Should compile Ruby code into ISeq cache?
+    compile_cache_iseq:   true,
+    # Should compile YAML into a cache?
+    compile_cache_yaml:   true
+  )
+end

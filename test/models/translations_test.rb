fb19f4d3f124d64bc8bebe0971a92202f241b422
David A. Wheeler <dwheeler@dwheeler.com>
2017-06-21 22:50:33 -0400

    Rename variables to make their purpose clearer
    Per review feedback from Dan Kohn.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- test/models/translations_test.rb
+++ test/models/translations_test.rb
@@ -23,17 +23,17 @@ class TranslationsTest < ActiveSupport::TestCase
   # Class can cause trouble, but we need it for glyphicons, etc.
   ACCEPTABLE_ATTRS = %w[href name class target].freeze
 
-  def sanitize_html(x)
+  def sanitize_html(text)
     html_sanitizer = Rails::Html::WhiteListSanitizer.new
     html_sanitizer.sanitize(
-      x, tags: ACCEPTABLE_TAGS,
-         attributes: ACCEPTABLE_ATTRS
+      text, tags: ACCEPTABLE_TAGS,
+            attributes: ACCEPTABLE_ATTRS
     ).to_s
   end
 
-  def regularize_html(x)
+  def regularize_html(text)
     regularizer = Rails::Html::TargetScrubber.new
-    Loofah.fragment(x).scrub!(regularizer).to_s
+    Loofah.fragment(text).scrub!(regularizer).to_s
   end
 
   # Return true if x is a "simple" (non-compound) non-string type
@@ -45,21 +45,21 @@ class TranslationsTest < ActiveSupport::TestCase
   # Return first unacceptable HTML in x (recursively), else (nil|false)
   # To recurse we really want kind_of?, not is_a?, so disable rubocop rule
   # rubocop:disable Style/ClassCheck, Metrics/MethodLength
-  def find_unacceptable_html(x)
-    if x.kind_of?(Array) || x.kind_of?(Hash)
-      x.find { |part| find_unacceptable_html(part) }
-    elsif x.kind_of?(String) # includes safe_html
-      return nil unless x.include?('<') # Can't be a problem, no '<'
-      # Text considered okay if the results of "sanitizing" it are
+  def find_unacceptable_html(translation)
+    if translation.kind_of?(Array) || translation.kind_of?(Hash)
+      translation.find { |part| find_unacceptable_html(part) }
+    elsif translation.kind_of?(String) # includes safe_html
+      return nil unless translation.include?('<') # Can't be a problem, no '<'
+      # Translation text is okay if the results of "sanitizing" it are
       # same as when we simply "regularize" the text without sanitizing it.
-      sanitized = sanitize_html(x)
-      regularized = regularize_html(x)
+      sanitized = sanitize_html(translation)
+      regularized = regularize_html(translation)
       if sanitized != regularized
         p "Unacceptable HTML.\nSan=<#{sanitized}>\nReg=<#{regularized}>"
       end
       sanitized != regularized
     else
-      !simple_type(x) # Require that it be simple: symbol, number, etc.
+      !simple_type(translation) # else must be simple: symbol, number, etc.
     end
   end
   # rubocop:enable Style/ClassCheck, Metrics/MethodLength

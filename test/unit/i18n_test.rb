11b4467d73b421025ab772523cd3a8e8c28a56f6
David A. Wheeler <dwheeler@dwheeler.com>
2017-06-24 15:29:01 -0400

    Simplify syntax of symbol
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -12,7 +12,7 @@ class I18nTest < ActiveSupport::TestCase
     assert_equal 'Hello world', I18n.t(:hello, locale: :en)
     assert_equal 'Bonjour le monde', I18n.t(:hello, locale: :fr)
     assert_equal '你好，世界', I18n.t(:hello, locale: :'zh-CN')
-    assert_equal 'Здравствуй, мир', I18n.t(:hello, locale: :'ru')
+    assert_equal 'Здравствуй, мир', I18n.t(:hello, locale: :ru)
     assert_equal 'Last translation entry', I18n.t(:last_entry, locale: :en)
   end
 end

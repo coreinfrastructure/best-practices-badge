401b46855fec2ac3204f4d4eadbf32405ff00f3d
David A. Wheeler <dwheeler@dwheeler.com>
2018-01-10 17:02:58 -0500

    Clean up CORS environment tests per review
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- test/unit/cors_library.rb
+++ test/unit/cors_library.rb
@@ -7,20 +7,18 @@
 require 'test_helper'
 
 class CorsLibraryTest < ActiveSupport::TestCase
-  test 'CORS library middleware stack location check in production' do
+  test 'CORS library middleware stack location check in environments' do
     # Because of the way it works, Rack::Cors *must* be first in the Rack
     # middleware stack, as documented here: https://github.com/cyu/rack-cors
     # This test verifies this precondition, because it'd be easy to
     # accidentally cause this assumption to fail as code is changed and
     # gems are added or updated.
-    first = `RAILS_ENV=production rake middleware | grep '^use ' | head -1`
-    first = first.chomp
-    assert_equal 'use Rack::Cors', first
-  end
-
-  test 'CORS library middleware stack location check in test environment' do
-    first = `RAILS_ENV=test rake middleware | grep '^use ' | head -1`
-    first = first.chomp
-    assert_equal 'use Rack::Cors', first
+    # This is a slow test (we bring up a whole environment), so we
+    # intentionally omit "development" from the list of tested environments.
+    %w[production test].each do |environment|
+      command = "RAILS_ENV=#{environment} rake middleware"
+      result = IO.popen(command).readlines.grep(/^use /).first.chomp
+      assert_equal 'use Rack::Cors', result
+    end
   end
 end

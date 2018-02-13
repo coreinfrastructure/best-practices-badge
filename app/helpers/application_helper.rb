abd71fde2a5d6faa6c5b57601d81539e1e0358ef
David A. Wheeler <dwheeler@dwheeler.com>
2018-02-13 00:12:18 -0500

    Create and use helper my_icon_tag
    Since icon tags show up in many places, create a helper to generate
    the tag and use it.  Putting the icon tag creation in one place
    should make it easier to change icon generation later.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,17 @@
 # SPDX-License-Identifier: MIT
 
 module ApplicationHelper
+  # Returns the tag for a given icon.
+  # In font-awesome 5, the category "fab" is used for brands,
+  # while "fas" is used for general (solid) icons.
+  # Currently we use an icon file; in the future we
+  # might use a reference to an SVG or SVG sprite.
+  def my_icon_tag(icon, category = 'fa')
+    # Unfortunately, rubocop doesn't realize that constants are safe,
+    # so we have to disable the OutputSafety check here.
+    # rubocop: disable Rails/OutputSafety
+    ('<i class="'.html_safe + category + ' '.html_safe + icon +
+      '" aria-hidden="true"></i>&nbsp;'.html_safe)
+    # rubocop: enable Rails/OutputSafety
+  end
 end

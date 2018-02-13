505971ed7858e25e3391baaf6263212848ee96c2
David A. Wheeler <dwheeler@dwheeler.com>
2018-02-13 10:44:13 -0500

    Optimize my_icon_tag routine
    This new routine gets called a lot.  Optimize its use of
    SafeBuffer so it doesn't create lots of transient temporaries.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,17 +5,28 @@
 # SPDX-License-Identifier: MIT
 
 module ApplicationHelper
-  # Returns the tag for a given icon.
+  # Unfortunately, rubocop doesn't realize that concatenating
+  # constants we define is safe.
+  # rubocop: disable Rails/OutputSafety
+  FA_HTML_SAFE = 'fa'.html_safe
+  # rubocop: enable Rails/OutputSafety
+
+  # Returns the tag for a given icon as a SafeBuffer.
   # In font-awesome 5, the category "fab" is used for brands,
   # while "fas" is used for general (solid) icons.
   # Currently we use an icon file; in the future we
   # might use a reference to an SVG or SVG sprite.
-  def my_icon_tag(icon, category = 'fa')
-    # Unfortunately, rubocop doesn't realize that constants are safe,
-    # so we have to disable the OutputSafety check here.
+  def my_icon_tag(icon, category = FA_HTML_SAFE)
+    # Unfortunately, rubocop doesn't realize that concatenating
+    # constants we define is safe.
     # rubocop: disable Rails/OutputSafety
-    ('<i class="'.html_safe + category + ' '.html_safe + icon +
-      '" aria-hidden="true"></i>&nbsp;'.html_safe)
+    res = ''.html_safe
+    res.safe_concat('<i class="')
+    res += category
+    res.safe_concat(' ')
+    res += icon
+    res.safe_concat('" aria-hidden="true"></i>&nbsp;')
     # rubocop: enable Rails/OutputSafety
+    res
   end
 end

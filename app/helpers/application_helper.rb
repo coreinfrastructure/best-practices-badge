6b6f2bff6982a726b3b7fde3224fc39a36b57b6e
David A. Wheeler <dwheeler@dwheeler.com>
2018-02-14 17:17:28 -0500

    Use more font awesome and tweak signup text (#1049)
    * Use more font awesome and tweak signup text
    
    This commit adds icons to all major navigation-level actions,
    and switches the ones that existed to all use font awesome.
    
    This improves the look, and is a step towards eliminating
    glyphicons and subsetting font awesome (to speed first-time display).
    
    It also tweaks the login and signup text to make it clearer that
    logging in via GitHub does an automatic sign-up.
    
    One challenge is that in English "sign in" and "sign up"
    are different operations, but they sound similar and are related,
    so it's easy for people to be confused.
    In addition, because we want users to have *convenient* experience,
    users who sign in using GitHub also have an automatic sign up
    (if necessary).  We tried to be clear before - hopefully these
    changes will make it even clearer.
    
    Since icon tags show up in many places, create a helper to generate
    the tag and use it.  Putting the icon tag creation in one place
    should make it easier to change icon generation later.
    This new routine gets called a lot.  Optimize its use of
    SafeBuffer so it doesn't create lots of transient temporaries.
    
    Here we try to keep the UI terminology consistent, by saying
    "custom" everywhere.
    
    We also tweak some other related text.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,28 @@
 # SPDX-License-Identifier: MIT
 
 module ApplicationHelper
+  # Unfortunately, rubocop doesn't realize that concatenating
+  # constants we define is safe.
+  # rubocop: disable Rails/OutputSafety
+  FA_HTML_SAFE = 'fa'.html_safe
+  # rubocop: enable Rails/OutputSafety
+
+  # Returns the tag for a given icon as a SafeBuffer.
+  # In font-awesome 5, the category "fab" is used for brands,
+  # while "fas" is used for general (solid) icons.
+  # Currently we use an icon file; in the future we
+  # might use a reference to an SVG or SVG sprite.
+  def my_icon_tag(icon, category = FA_HTML_SAFE)
+    # Unfortunately, rubocop doesn't realize that concatenating
+    # constants we define is safe.
+    # rubocop: disable Rails/OutputSafety
+    res = ''.html_safe
+    res.safe_concat('<i class="')
+    res += category
+    res.safe_concat(' ')
+    res += icon
+    res.safe_concat('" aria-hidden="true"></i>&nbsp;')
+    # rubocop: enable Rails/OutputSafety
+    res
+  end
 end

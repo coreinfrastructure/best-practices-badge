035e94a54af69cac453590b9bd2d4bf32d336c83
David A. Wheeler <dwheeler@dwheeler.com>
2018-05-22 23:57:10 -0400

    Load keys for encrypting email addresses
    Load the secret keys for encrypting email addresses during
    initialization, so that we can encrypt email addresses.
    We'll set a default key value, so that we can easily
    create tests.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ config/initializers/encrypt_email_addresses.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Copyright 2015-2017, the Linux Foundation, IDA, and the
+# CII Best Practices badge contributors
+# SPDX-License-Identifier: MIT
+
+# Setup the keys for hashing and encrypting email addresses.
+# These keys protect the data at rest (e.g., so that user addresses
+# are not immediately revealed in backups).
+
+BITS_PER_HEX_DIGIT = 4
+
+Rails.application.config.assets.email_hash_key =
+  ENV['EMAIL_HASH_KEY'] || '0' * (128 / BITS_PER_HEX_DIGIT)
+
+Rails.application.config.assets.email_encrypted_key =
+  ENV['EMAIL_ENCRYPTED_KEY'] || '0' * (256 / BITS_PER_HEX_DIGIT)

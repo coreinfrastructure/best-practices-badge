7542aabd872ccdcf71fcde8746c358aa35e1c66f
David A. Wheeler <dwheeler@dwheeler.com>
2018-05-29 17:54:41 -0400

    Add "column:" index values so this migration is reversible
    Add "column:" information for the indexes so that this
    migration is reversible.
    
    In general, we try to make migrations reversible - doing so
    makes it much easier to deal with unexpected problems.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- db/migrate/20180529212405_remove_unencrypted_email_from_users.rb
+++ db/migrate/20180529212405_remove_unencrypted_email_from_users.rb
@@ -2,8 +2,10 @@
 
 class RemoveUnencryptedEmailFromUsers < ActiveRecord::Migration[5.1]
   def change
-    remove_index :users, name: 'index_users_on_unencrypted_email'
+    remove_index :users, name: 'index_users_on_unencrypted_email',
+                 column: 'unencrypted_email'
     remove_index :users,
+                 column: 'unencrypted_email',
                  name: 'unique_local_email', unique: true,
                  where: "((provider)::text = 'local'::text)"
     remove_column :users, :unencrypted_email, :citext

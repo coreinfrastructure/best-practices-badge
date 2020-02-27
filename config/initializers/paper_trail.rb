61c5c2ed69088fb7e1d0c1908c7cf13b9b949ab1
David A. Wheeler <dwheeler@dwheeler.com>
2020-02-27 16:37:10 -0500

    Update gem paper_trail (9.0.1->10.3.1) + tests (#1409)
    This updates the gem paper_trail (9.0.1->10.3.1).
    This gem records previous versions of project entries so that we
    can see what older versions looked like.
    
    This also adds a test to check that paper_trail is actually
    properly recording project changes.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -4,4 +4,9 @@
 # CII Best Practices badge contributors
 # SPDX-License-Identifier: MIT
 
-PaperTrail.config.track_associations = false
+# Association Tracking for PaperTrail has been extracted to a separate gem
+# as of PaperTrail version 10.
+# We could use it by adding `paper_trail-association_tracking` to the Gemfile.
+# However, like most people, we don't use it.
+# Thus, we no longer need to add this line:
+# PaperTrail.config.track_associations = false

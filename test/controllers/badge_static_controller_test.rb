3fd54ed96a78425d95b9aca2a3b77ebb36a90f1d
David A. Wheeler <dwheeler@dwheeler.com>
2020-10-10 00:13:22 -0400

    Add a new /badge_static/:value route
    Add a new /badge_static/:value route; this serves *static* SVG
    badge images given just "value". Value can be 0..99, passing,
    silver, or gold. This will be cached by the CDN (Fastly).
    
    This is the first step in implementing some countermeasures against
    CDN race conditions. The plan is to use this to
    serve /:locale/projects/:number badge entries. That way, if the
    badge entry has just changed, but the CDN hasn't caught up yet, the
    user who just change the project data will nevertheless
    see the *current* status of the project.
    We could also do this with the /projects display.
    
    We still want projects, on their README, to use the
    /projects/:number/badge URL when displaying badges, because that
    will automatically adjust as the project's badge status changes.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ test/controllers/badge_static_controller_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+# Copyright the Linux Foundation and the
+# CII Best Practices badge contributors
+# SPDX-License-Identifier: MIT
+
+require 'test_helper'
+
+class BadgeStaticControllerTest < ActionDispatch::IntegrationTest
+  test 'get badge static image for 0' do
+    get '/badge_static/0'
+    assert_response :success
+    assert_includes @response.body, '<svg xmlns='
+    assert_includes @response.body, 'in progress 0%'
+    assert_not_includes @response.body, 'gold'
+  end
+
+  test 'get badge static image for 99' do
+    get '/badge_static/99'
+    assert_response :success
+    assert_includes @response.body, '<svg xmlns='
+    assert_includes @response.body, 'in progress 99%'
+    assert_not_includes @response.body, 'gold'
+  end
+
+  test 'get badge static image for passing' do
+    get '/badge_static/passing'
+    assert_response :success
+    assert_includes @response.body, '<svg xmlns='
+    assert_includes @response.body, 'passing'
+    assert_not_includes @response.body, 'gold'
+  end
+
+  test 'get badge static image for silver' do
+    get '/badge_static/silver'
+    assert_response :success
+    assert_includes @response.body, '<svg xmlns='
+    assert_includes @response.body, 'silver'
+    assert_not_includes @response.body, 'gold'
+  end
+
+  test 'get badge static image for gold' do
+    get '/badge_static/gold'
+    assert_response :success
+    assert_includes @response.body, '<svg xmlns='
+    assert_includes @response.body, 'gold'
+    assert_not_includes @response.body, 'silver'
+    assert_not_includes @response.body, 'passing'
+  end
+
+  test 'cannot get badge static image for bad value' do
+    get '/badge_static/0q'
+    assert_response :not_found
+    assert_includes @response.body, 'Sorry, '
+    assert_not_includes @response.body, '0q'
+  end
+end

2f0d47ade8000bc40e3006d8f8c8d13efc014f01
David A. Wheeler <dwheeler@dwheeler.com>
2020-12-21 22:09:26 -0500

    app/models/bad_password.rb: Minor cleanup
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/models/bad_password.rb
+++ app/models/bad_password.rb
@@ -7,8 +7,8 @@
 # "known bad passwords". There's not anything to it; ActiveRecord handles it.
 
 class BadPassword < ApplicationRecord
-  # Force load into the database the list of bad passwords.
-  def self.force_load
+  # rubocop:disable Metrics/MethodLength
+  def self.bad_passwords_from_file
     require 'zlib'
     bad_password_array = []
     Zlib::GzipReader.open('raw-bad-passwords-lowercase.txt.gz') do |gz|
@@ -16,7 +16,14 @@ class BadPassword < ApplicationRecord
         bad_password_array.push({ forbidden: line.chomp.downcase.freeze })
       end
     end
+    bad_password_array
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  # Force load into the database the list of bad passwords.
+  def self.force_load
     BadPassword.delete_all
+    bad_password_array = bad_passwords_from_file
     # Update all in one transaction, or it will take a *long* time
     transaction do
       # TODO: Speed this up with Rails 6's "insert_all!" (bulk insert)

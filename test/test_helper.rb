28840e003bf0cca07fcd81be40d2bcc1fa25849a
David A. Wheeler <dwheeler@dwheeler.com>
2021-01-08 10:56:29 -0500

    Restore comented-out lines
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -98,212 +98,212 @@ require 'selenium/webdriver'
 require 'webdrivers'
 
 # Register "chrome" driver - use it via Selenium.
-# Capybara.register_driver :chrome do |app|
-#   Capybara::Selenium::Driver.new(app, browser: :chrome)
-# end
-# 
-# # Register "headless_chrome" driver - use it via Selenium.
-# # The configuration approach documented here isn't actually headless:
-# # https://robots.thoughtbot.com/headless-feature-specs-with-chrome
-# # So we instead use the approach documented in:
-# # https://github.com/teamcapybara/capybara/blob/master/spec/
-# # selenium_spec_chrome.rb#L6
-# Capybara.register_driver :headless_chrome do |app|
-#   browser_options = Selenium::WebDriver::Chrome::Options.new
-#   browser_options.binary = ENV.fetch('GOOGLE_CHROME_SHIM', nil) if ENV['CI']
-#   browser_options.args << '--headless'
-#   browser_options.args << '--disable-gpu' if Gem.win_platform?
-#   driver = Capybara::Selenium::Driver.new(
-#     app, browser: :chrome, options: browser_options
-#   )
-#   driver.browser.download_path = Capybara.save_path
-#   driver
-# end
-# 
-# # Note that DRIVER only controls the Capybara javascript_driver.
-# driver = ENV['DRIVER'].try(:to_sym)
-# Capybara.javascript_driver = driver.present? ? driver : :headless_chrome
-# 
-# # By default newer versions of Capybara have the annoying habit of
-# # sending this in the middle of a test:
-# # > Capybara starting Puma...
-# # > * Version 3.12.2 , codename: Llamas in Pajamas
-# # > * Min threads: 0, max threads: 4
-# # > * Listening on tcp://127.0.0.1:31337
-# # This makes it hard to see the test status, so quiet it per:
-# # Capybara.server = :puma, { Silent: true }
-# # NOTE: This forces Capybara's server to be Puma; if the production server
-# # is something else, you might want to change this. For more info, see:
-# # https://github.com/rails/rails/issues/28109
-# # https://github.com/rspec/rspec-rails/issues/1897
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+# Register "headless_chrome" driver - use it via Selenium.
+# The configuration approach documented here isn't actually headless:
+# https://robots.thoughtbot.com/headless-feature-specs-with-chrome
+# So we instead use the approach documented in:
+# https://github.com/teamcapybara/capybara/blob/master/spec/
+# selenium_spec_chrome.rb#L6
+Capybara.register_driver :headless_chrome do |app|
+  browser_options = Selenium::WebDriver::Chrome::Options.new
+  browser_options.binary = ENV.fetch('GOOGLE_CHROME_SHIM', nil) if ENV['CI']
+  browser_options.args << '--headless'
+  browser_options.args << '--disable-gpu' if Gem.win_platform?
+  driver = Capybara::Selenium::Driver.new(
+    app, browser: :chrome, options: browser_options
+  )
+  driver.browser.download_path = Capybara.save_path
+  driver
+end
+
+# Note that DRIVER only controls the Capybara javascript_driver.
+driver = ENV['DRIVER'].try(:to_sym)
+Capybara.javascript_driver = driver.present? ? driver : :headless_chrome
+
+# By default newer versions of Capybara have the annoying habit of
+# sending this in the middle of a test:
+# > Capybara starting Puma...
+# > * Version 3.12.2 , codename: Llamas in Pajamas
+# > * Min threads: 0, max threads: 4
+# > * Listening on tcp://127.0.0.1:31337
+# This makes it hard to see the test status, so quiet it per:
 # Capybara.server = :puma, { Silent: true }
-# 
-# # NOTE: We *could* speed up test execution by disabling PaperTrail
-# # except in cases where we check PaperTrail results. PaperTrail records all
-# # project creation and change events (enabling you to see older versions),
-# # so in some cases Papertrail slows tests slightly.  To do this, see:
-# # https://github.com/paper-trail-gem/paper_trail
-# # However, we have intentionally chosen to *not* do that.
-# # Where reasonable we have tried to keep the test environment
-# # the *same* as the production environment where it's reasonable to do so;
-# # every difference can hide a problem from our tests.
-# # The tests run fast enough as it is, and avoiding problems due to such
-# # differences is more important. At the least, we're making sure that
-# # PaperTrail doesn't cause a crash in the various tests, and that's worth
-# # checking.
-# 
-# module ActiveSupport
-#   # rubocop: disable Metrics/ClassLength
-#   class TestCase
-#     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
-#     # order.
-#     ActiveRecord::Migration.maintain_test_schema!
-#     self.use_transactional_tests = true
-#     fixtures :all
-# 
-#     # Add more helper methods to be used by all tests here...
-# 
-#     def configure_omniauth_mock(cassette = 'github_login')
-#       OmniAuth.config.test_mode = true
-#       OmniAuth.config.add_mock(:github, omniauth_hash(cassette))
-#     end
-# 
-#     def contents(file_name)
-#       IO.read "test/fixtures/files/#{file_name}"
-#     end
-# 
-#     # rubocop:disable Metrics/MethodLength
-#     def kill_sticky_headers
-#       # https://alisdair.mcdiarmid.org/kill-sticky-headers/
-#       script = <<-JAVASCRIPT_KILL_STICKY_HEADERS
-#       (function () {
-#         var i, elements = document.querySelectorAll('body *');
-# 
-#         for (i = 0; i < elements.length; i++) {
-#           if (getComputedStyle(elements[i]).position === 'fixed') {
-#             elements[i].parentNode.removeChild(elements[i]);
-#           }
-#         }
-#       })();
-#       JAVASCRIPT_KILL_STICKY_HEADERS
-#       execute_script script
-#     end
-#     # rubocop:enable Metrics/MethodLength
-# 
-#     # rubocop:disable Metrics/MethodLength
-#     # Note: In many tests we use "password" as the password.
-#     # Users can (no longer) create accounts with this too-easy password.
-#     # Using "password" helps test that users can log in to their
-#     # existing accounts, even if we make the password rules harsher later.
-#     def log_in_as(
-#       user, password: 'password', provider: 'local', remember_me: '1',
-#       time_last_used: Time.now.utc
-#     )
-#       # This is based on "Ruby on Rails Tutorial" by Michael Hargle, chapter 8,
-#       # https://www.railstutorial.org/book
-#       if integration_test?
-#         post login_path, params: {
-#           session: {
-#             email:  user.email, password: password,
-#             provider: provider, remember_me: remember_me,
-#             time_last_used: time_last_used
-#           }
-#         }
-#         # Do this instead, it at least checks the password:
-#         # session[:user_id] = user.id if user.try(:authenticate, password)
-#       else
-#         session[:user_id] = user.id
-#         session[:time_last_used] = time_last_used
-#       end
-#     end
-#     # rubocop:enable Metrics/MethodLength
-# 
-#     def scroll_to_see(id)
-#       # From http://toolsqa.com/selenium-webdriver/scroll-element-view-selenium-javascript/
-#       execute_script("document.getElementById('#{id}').scrollIntoView(false);")
-#     end
-# 
-#     def user_logged_in?
-#       # Returns true if a test user is logged in.
-#       !session[:user_id].nil?
-#     end
-# 
-#     def wait_for_jquery
-#       Timeout.timeout(Capybara.default_max_wait_time) do
-#         loop until finished_all_jquery_requests?
-#       end
-#     end
-# 
-#     def wait_for_url(url)
-#       Timeout.timeout(Capybara.default_max_wait_time) do
-#         loop do
-#           uri = URI.parse(current_url)
-#           break if url == "#{uri.path}?#{uri.query}"
-#         end
-#       end
-#     end
-# 
-#     private
-# 
-#     def finished_all_jquery_requests?
-#       evaluate_script('jQuery.active').zero?
-#     end
-# 
-#     def integration_test?
-#       # Returns true inside an integration test.
-#       # Based on "Ruby on Rails Tutorial" by Michael Hargle, chapter 8,
-#       # https://www.railstutorial.org/book
-#       defined?(post_via_redirect)
-#     end
-# 
-#     def omniauth_hash(cassette)
-#       {
-#         'provider' => 'github',
-#         'uid' => '12345',
-#         'credentials' => { 'token' => vcr_oauth_token(cassette) },
-#         'info' => {
-#           'name' => 'CII Test',
-#           'email' => 'test@example.com',
-#           'nickname' => 'ciitest'
-#         }
-#       }
-#     end
-# 
-#     def vcr_oauth_token(cassette)
-#       github_login_vcr_file = "test/vcr_cassettes/#{cassette}.yml"
-#       return Null unless File.exist?(github_login_vcr_file)
-# 
-#       y = YAML.load_file(github_login_vcr_file)
-#               .with_indifferent_access
-#       query_string = y[:http_interactions][0][:response][:body][:string]
-#       Rack::Utils.parse_nested_query(query_string)['access_token']
-#     end
-# 
-#     def key_with_nil_value(hash)
-#       hash.each do |k, v|
-#         return k.to_s if v.nil?
-#         next unless v.is_a?(Hash)
-# 
-#         nil_key = key_with_nil_value(v)
-#         next if nil_key == ''
-# 
-#         return "#{k}.#{nil_key}"
-#       end
-#       ''
-#     end
-# 
-#     # Re-implement assert_select - return true iff a CSS selection
-#     # using *selector* contains exactly "contents".
-#     # The problem is that assert_select fails oddly when running a global
-#     # "rails test" (though it works fine if running "rails test FILENAME").
-#     # To solve this, we re-implement assert_select so we have a working version.
-#     def my_assert_select(selector, contents)
-#       results = css_select(selector)
-#       results.each do |selection|
-#         return true if selection.content == contents
-#       end
-#       false
-#     end
-#   end
-#   # rubocop: enable Metrics/ClassLength
-# end
+# NOTE: This forces Capybara's server to be Puma; if the production server
+# is something else, you might want to change this. For more info, see:
+# https://github.com/rails/rails/issues/28109
+# https://github.com/rspec/rspec-rails/issues/1897
+Capybara.server = :puma, { Silent: true }
+
+# NOTE: We *could* speed up test execution by disabling PaperTrail
+# except in cases where we check PaperTrail results. PaperTrail records all
+# project creation and change events (enabling you to see older versions),
+# so in some cases Papertrail slows tests slightly.  To do this, see:
+# https://github.com/paper-trail-gem/paper_trail
+# However, we have intentionally chosen to *not* do that.
+# Where reasonable we have tried to keep the test environment
+# the *same* as the production environment where it's reasonable to do so;
+# every difference can hide a problem from our tests.
+# The tests run fast enough as it is, and avoiding problems due to such
+# differences is more important. At the least, we're making sure that
+# PaperTrail doesn't cause a crash in the various tests, and that's worth
+# checking.
+
+module ActiveSupport
+  # rubocop: disable Metrics/ClassLength
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
+    # order.
+    ActiveRecord::Migration.maintain_test_schema!
+    self.use_transactional_tests = true
+    fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+
+    def configure_omniauth_mock(cassette = 'github_login')
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.add_mock(:github, omniauth_hash(cassette))
+    end
+
+    def contents(file_name)
+      IO.read "test/fixtures/files/#{file_name}"
+    end
+
+    # rubocop:disable Metrics/MethodLength
+    def kill_sticky_headers
+      # https://alisdair.mcdiarmid.org/kill-sticky-headers/
+      script = <<-JAVASCRIPT_KILL_STICKY_HEADERS
+      (function () {
+        var i, elements = document.querySelectorAll('body *');
+
+        for (i = 0; i < elements.length; i++) {
+          if (getComputedStyle(elements[i]).position === 'fixed') {
+            elements[i].parentNode.removeChild(elements[i]);
+          }
+        }
+      })();
+      JAVASCRIPT_KILL_STICKY_HEADERS
+      execute_script script
+    end
+    # rubocop:enable Metrics/MethodLength
+
+    # rubocop:disable Metrics/MethodLength
+    # Note: In many tests we use "password" as the password.
+    # Users can (no longer) create accounts with this too-easy password.
+    # Using "password" helps test that users can log in to their
+    # existing accounts, even if we make the password rules harsher later.
+    def log_in_as(
+      user, password: 'password', provider: 'local', remember_me: '1',
+      time_last_used: Time.now.utc
+    )
+      # This is based on "Ruby on Rails Tutorial" by Michael Hargle, chapter 8,
+      # https://www.railstutorial.org/book
+      if integration_test?
+        post login_path, params: {
+          session: {
+            email:  user.email, password: password,
+            provider: provider, remember_me: remember_me,
+            time_last_used: time_last_used
+          }
+        }
+        # Do this instead, it at least checks the password:
+        # session[:user_id] = user.id if user.try(:authenticate, password)
+      else
+        session[:user_id] = user.id
+        session[:time_last_used] = time_last_used
+      end
+    end
+    # rubocop:enable Metrics/MethodLength
+
+    def scroll_to_see(id)
+      # From http://toolsqa.com/selenium-webdriver/scroll-element-view-selenium-javascript/
+      execute_script("document.getElementById('#{id}').scrollIntoView(false);")
+    end
+
+    def user_logged_in?
+      # Returns true if a test user is logged in.
+      !session[:user_id].nil?
+    end
+
+    def wait_for_jquery
+      Timeout.timeout(Capybara.default_max_wait_time) do
+        loop until finished_all_jquery_requests?
+      end
+    end
+
+    def wait_for_url(url)
+      Timeout.timeout(Capybara.default_max_wait_time) do
+        loop do
+          uri = URI.parse(current_url)
+          break if url == "#{uri.path}?#{uri.query}"
+        end
+      end
+    end
+
+    private
+
+    def finished_all_jquery_requests?
+      evaluate_script('jQuery.active').zero?
+    end
+
+    def integration_test?
+      # Returns true inside an integration test.
+      # Based on "Ruby on Rails Tutorial" by Michael Hargle, chapter 8,
+      # https://www.railstutorial.org/book
+      defined?(post_via_redirect)
+    end
+
+    def omniauth_hash(cassette)
+      {
+        'provider' => 'github',
+        'uid' => '12345',
+        'credentials' => { 'token' => vcr_oauth_token(cassette) },
+        'info' => {
+          'name' => 'CII Test',
+          'email' => 'test@example.com',
+          'nickname' => 'ciitest'
+        }
+      }
+    end
+
+    def vcr_oauth_token(cassette)
+      github_login_vcr_file = "test/vcr_cassettes/#{cassette}.yml"
+      return Null unless File.exist?(github_login_vcr_file)
+
+      y = YAML.load_file(github_login_vcr_file)
+              .with_indifferent_access
+      query_string = y[:http_interactions][0][:response][:body][:string]
+      Rack::Utils.parse_nested_query(query_string)['access_token']
+    end
+
+    def key_with_nil_value(hash)
+      hash.each do |k, v|
+        return k.to_s if v.nil?
+        next unless v.is_a?(Hash)
+
+        nil_key = key_with_nil_value(v)
+        next if nil_key == ''
+
+        return "#{k}.#{nil_key}"
+      end
+      ''
+    end
+
+    # Re-implement assert_select - return true iff a CSS selection
+    # using *selector* contains exactly "contents".
+    # The problem is that assert_select fails oddly when running a global
+    # "rails test" (though it works fine if running "rails test FILENAME").
+    # To solve this, we re-implement assert_select so we have a working version.
+    def my_assert_select(selector, contents)
+      results = css_select(selector)
+      results.each do |selection|
+        return true if selection.content == contents
+      end
+      false
+    end
+  end
+  # rubocop: enable Metrics/ClassLength
+end

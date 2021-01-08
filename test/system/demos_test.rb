eb6161ea15d2c856bf62210390d6f1da61606ea2
David A. Wheeler <dwheeler@dwheeler.com>
2021-01-08 10:35:12 -0500

    Generate system test stubs
    Record results of:
    
    > bin/rails generate system_test demo
    
    This resulted in:
    * create    test/application_system_test_case.rb
    * create    test/system/demos_test.rb
    
    So we have stub Rails 6 system tests to start with.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ test/system/demos_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class DemosTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit demos_url
+  #
+  #   assert_selector "h1", text: "Demo"
+  # end
+end

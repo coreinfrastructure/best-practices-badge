bc6bc7f39855e257c70573b4f889ca29511f5810
David A. Wheeler <dwheeler@linuxfoundation.org>
2021-05-10 10:34:56 -0400

    Update gem chartkick (3.4.2->4.0.4) (#1619)
    Update gem chartkick (3.4.2->4.0.4). This required some code changes:
    * 'defer' is now the default and the system complains if you use it.
      We were already using it, so since it's now the default
      don't request it.
    * The options hash format has changed slightly, so we had to change
      how it's called. We need to this to show x-axis dates in ISO 8601
      (international) format. Comments were added to point to the
      hard-to-find information on how to do this :-).
    
    This commit also adds comments about what must be changed if
    we started using webpacker.  I thought it'd be good to warn
    about these changes, to hopefully reduce mysteries in the future
    if we chose to do that.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/assets/javascripts/project-stats.js
+++ app/assets/javascripts/project-stats.js
@@ -1,5 +1,6 @@
 // Chart bundle is requires a lot of evaluations on page load so lets only
 // load it on the page it is needed.
+// Note: if you use webpacker, change to require "chartkick/chart.js"
 // Chart creation via chartkick:
 //= require Chart.bundle
 //= require chartkick

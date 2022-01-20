3369bebac85dcf3d75a5c4354a9d5225e3ff001f
David A. Wheeler <dwheeler@dwheeler.com>
2022-01-20 11:49:52 -0500

    Update Hyperledger logo
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,23 @@
 
 module ApplicationHelper
   include Pagy::Frontend
+
+  # Approach from https://blog.cloud66.com/using-svgs-in-a-rails-stack/
+  # "Using SVGs in a Rails Stack" by
+  # Khash SajadiKhash Sajadi 09 MARCH 2021
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    asset = assets.find_asset(filename)
+
+    if asset
+      file = asset.source.force_encoding("UTF-8")
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "svg"
+      svg["class"] = options[:class] if options[:class].present?
+    else
+      doc = "<!-- SVG #{filename} not found -->"
+    end
+
+    raw doc
+  end
 end

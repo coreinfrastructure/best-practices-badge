27f28a10929e8440e2ca2fa8b538e4c3bb1b3532
Andrew Fader <fader@yagni.co>
2024-12-18 18:41:04 -0500

    only load monkeypatch in test and dev
    Signed-off-by: Andrew Fader <fader@yagni.co>

--- config/initializers/monkeypatch_vcr.rb
+++ config/initializers/monkeypatch_vcr.rb
@@ -1,29 +1,31 @@
 # frozen_string_literal: true
 
-# https://github.com/vcr/vcr/blob/master/lib/vcr/library_hooks/webmock.rb
+if Rails.env.test? || Rails.env.development?
+  # https://github.com/vcr/vcr/blob/master/lib/vcr/library_hooks/webmock.rb
 
-module VCR
-  class LibraryHooks
-    module WebMock
-      module_function
+  module VCR
+    class LibraryHooks
+      module WebMock
+        module_function
 
-      def with_global_hook_disabled(request)
-        global_hook_disabled_requests << request
+        def with_global_hook_disabled(request)
+          global_hook_disabled_requests << request
 
-        begin
-          yield
-        ensure
-          global_hook_disabled_requests.delete(request)
+          begin
+            yield
+          ensure
+            global_hook_disabled_requests.delete(request)
+          end
         end
-      end
 
-      def global_hook_disabled?(request)
-        requests = Thread.current[:_vcr_webmock_disabled_requests]
-        requests&.include?(request)
-      end
+        def global_hook_disabled?(request)
+          requests = Thread.current[:_vcr_webmock_disabled_requests]
+          requests&.include?(request)
+        end
 
-      def global_hook_disabled_requests
-        Thread.current[:_vcr_webmock_disabled_requests] ||= []
+        def global_hook_disabled_requests
+          Thread.current[:_vcr_webmock_disabled_requests] ||= []
+        end
       end
     end
   end

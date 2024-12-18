fdc0158597eaef2eee3f82b9ca509daa73f592ea
David A. Wheeler <dwheeler@dwheeler.com>
2024-12-18 17:58:34 -0500

    Record updated db/schema.rb for 7.1
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_18_182207) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_18_182207) do
+  create_schema "heroku_ext"
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_stat_statements"

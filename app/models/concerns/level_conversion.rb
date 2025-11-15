a0898a18fc0ff6842ded7a92a0f53989963803d4
David A. Wheeler <dwheeler@dwheeler.com>
2025-11-15 15:09:27 -0500

    IDA wasn't involved in this file
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- app/models/concerns/level_conversion.rb
+++ app/models/concerns/level_conversion.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright the Linux Foundation, IDA, and the
+# Copyright the Linux Foundation and the
 # OpenSSF Best Practices badge contributors
 # SPDX-License-Identifier: MIT
 

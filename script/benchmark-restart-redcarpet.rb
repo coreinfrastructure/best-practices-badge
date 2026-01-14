c01bad94f02ebe56d717153f767ac52a01ee7e0c
David A. Wheeler <dwheeler@dwheeler.com>
2026-01-14 09:24:08 -0500

    Re-instantiate to eliminate redcarpet crash (#2624)
    We have gotten rare redcarpet crashes. After investigating
    alternatives, I've found a strong solution: re-instantiate
    the redcarpet processor each time before using it.
    We'll also continue to force it to use 1 thread at a time,
    to eliminate any possibility of multi-threading being a problem.
    
    This has surprisingly small overhead, and given the information
    we have, it should completely eliminate the problem.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ script/benchmark-restart-redcarpet.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+# Benchmark the performance overhead of recreating a Redcarpet markdown
+# processor on every request, instead of reusing it.
+
+require 'benchmark'
+
+require_relative '../config/environment'
+
+puts 'Benchmarking performance overhead of recreating Redcarpet markdown'
+puts 'processor on every request, instead of reusing it.'
+
+# The options we use, so we're doing a fair analysis.
+REDCARPET_MARKDOWN_RENDERER_OPTIONS = {
+  filter_html: true, no_images: true,
+    no_styles: true, safe_links_only: true,
+    link_attributes: { rel: 'nofollow ugc noopener noreferrer' }
+}.freeze
+
+REDCARPET_MARKDOWN_PROCESSOR_OPTIONS = {
+  no_intra_emphasis: true, autolink: true,
+  space_after_headers: true, fenced_code_blocks: true
+}.freeze
+
+# Quick test to ensure it works, and warm up things
+text = "# Hello World\nThis is a **test**."
+# The warning we see is processing; creating the renderer is a one-time
+# event that doesn't care about inputs
+RENDERER = Redcarpet::Render::HTML.new(REDCARPET_MARKDOWN_RENDERER_OPTIONS)
+markdown = Redcarpet::Markdown.new(RENDERER, REDCARPET_MARKDOWN_PROCESSOR_OPTIONS)
+
+puts "First markdown = #{markdown}\n"
+
+n = 10_000
+Benchmark.bm do |x|
+  x.report('Reused: ') { n.times { markdown.render(text) } }
+  # Re-create the markdown processor (but not the renderer) on each request
+  x.report('New Instance: ') { n.times { Redcarpet::Markdown.new(RENDERER, REDCARPET_MARKDOWN_PROCESSOR_OPTIONS).render(text) } }
+end

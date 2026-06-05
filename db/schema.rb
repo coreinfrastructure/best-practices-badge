80c62bae0600df734d6e299abd1868a7b86f00b5
David A. Wheeler <dwheeler@dwheeler.com>
2026-06-05 07:57:58 -0400

    Update schema.rb after dubious URL cleanup migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_02_200801) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_03_010000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
